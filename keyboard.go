a9648ef409e50a50a9f8d04c7f4f3f6415e121f7
alvin_su <alvin_su@outlook.com>
2022-06-06 11:57:56 +0800

    Update keyboard.go
    添加注释

--- keyboard.go
+++ keyboard.go
@@ -1,3 +1,4 @@
+//从键盘获取用户的输入
 package keyboard
 
 import (
@@ -6,7 +7,7 @@ import (
 	"strconv"
 	"strings"
 )
-
+//将键盘中用户的输入去掉空格并转为float返回
 func GetFloat() (float64, error) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
